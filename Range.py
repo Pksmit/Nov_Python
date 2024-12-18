636f29b85fd4fc3abe7737df00d4281bfb2248e3
Pksmit <viveksmith2942@gmail.com>
2024-12-18 08:55:11 +0530

    Update Range.py

--- Range.py
+++ Range.py
@@ -1,8 +1,11 @@
 count = 0
 sum = 0
+mul = 1
 for x in range(1,10,2):
     print(x)
     count=count+1
     sum = sum+x
+    mul = mul*x
 print("Total odd num:",count)
 print("Sum of odd num:",sum)
+print("Mul of odd num:",mul)
