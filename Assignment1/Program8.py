9148754e64661d0d9f2ff0ac6f7615dd7814b060
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:03:37 +0530

    Create Program8.py

---
+++ Assignment1/Program8.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first no:"))
+num2 = int(input("Enter second no:"))
+print("Numbers before swapping : ",num1,num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("Numbers after swapping : ",num1,num2)
