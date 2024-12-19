fdf03f264c0e305a2876c7b2f7e47e3d3e7dba8f
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:04:09 +0530

    Create Program9.py

---
+++ Assignment2/Program9.py
@@ -0,0 +1,18 @@
+day = int(input("Enter no between 1-7 : "))
+if day == 1:
+    print("Monday")
+elif day == 2: 
+    print("Tuesday") 
+elif day == 3: 
+    print("Wednesday")
+elif day == 4:  
+    print("Thursday") 
+elif day == 5: 
+    print("Friday") 
+elif day == 6: 
+    print("Satday") 
+elif day== 7:
+    print("Sunday") 
+else:
+    print("Invalid Vlaue")
+   
