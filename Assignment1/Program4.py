e44b4285452caa5daf4f272da3f4d6cebaed79da
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:03:54 +0530

    Create Program4.py

---
+++ Assignment1/Program4.py
@@ -0,0 +1,5 @@
+principal_amount = float(input("Enter value of principal_amount : "))
+rate_of_interest = float(input("Enter value of  rate_of_interest : "))
+time = float(input("Enter time in years : "))
+simple_interest = (principal_amount * rate_of_interest * time)/100 
+print("Simple interest : ",simple_interest)
