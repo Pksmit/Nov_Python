4ac4d1f4ebaadf36e2671f93120a91cd63d2dea3
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:04:03 +0530

    Create Program5.py

---
+++ Assignment1/Program5.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter first no : "))
+num2 = int(input("Enter second no : "))
+num3 = int(input("Enter third no : "))
+avg = (num1 + num2 + num3)/3
+print("Average of the numbers : ",avg)
