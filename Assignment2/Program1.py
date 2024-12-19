f70d25cb51360faa74a191c4997e491208412dfd
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:03:29 +0530

    Create Program1.py

---
+++ Assignment2/Program1.py
@@ -0,0 +1,7 @@
+num = int(input("Enter any number : "))
+if num<0:
+    print("The number ",num," is negative")
+elif num>0:
+    print("The number ",num," is positive")
+else:
+    print("The number ",num," is zero")
