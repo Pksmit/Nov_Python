dd64d6b43e21499f185cb75fef17fce9bf94b464
Pksmit <viveksmith2942@gmail.com>
2024-12-19 08:03:47 +0530

    Create Program8.py

---
+++ Assignment2/Program8.py
@@ -0,0 +1,5 @@
+num = int(input("Enter any no : "))
+if num % 5 == 0 and num % 11 == 0:
+    print("The num ",num," is divisible by both 5 and 11.")
+else:
+    print("The num ",num," is not divisible by both 5 and 11.")
