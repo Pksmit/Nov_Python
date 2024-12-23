a0da718ea56eccdf3bc60df068f1879184fe4404
Pksmit <viveksmith2942@gmail.com>
2024-12-23 19:24:36 +0530

    Create Program4.py

---
+++ Assignment3/Program4.py
@@ -0,0 +1,12 @@
+print("1. Red")
+print("2. Yellow")
+print("3. Green")
+choice = int(input("Choose traffic light colour :"))
+if choice == 1:
+    print("Next traffic light colour will be : GREEN🚦")
+elif choice == 2:
+    print("Next traffic light colour will be : RED🚦")
+elif choice == 3:
+    print("Next traffic light colour will be : YELLOW🚦")
+else:
+    print("Invalid!!!")
