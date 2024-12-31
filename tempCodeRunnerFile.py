26728459162437910352dfa2ba36498411a20059
Pksmit <viveksmith2942@gmail.com>
2024-12-31 09:03:45 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,27 +1,2 @@
-for row in range(5):
-    for col in range(row+1):
-        print(row,end=' ')
-    print()
-
-for row in range(5):
-    for col in range(row+1):
-        print(col,end=' ')
-    print()
-    
-for x in range(3):
-    for y in range(2):
-        print('*')
-        
-for x in range(3):
-    for y in range(2):
-        print('*',end=' ')
-        
-for row in range(4):
-    for col in range(5):
-        print('*',end=' ')
-    print()
-    
-for row in range(4):
-    for col in range(row+1):
-        print('*',end=' ')
-    print()
+if x in 
+# print(dup_list)
